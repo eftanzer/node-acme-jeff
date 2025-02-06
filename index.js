1e6443d17ebd8fafff1eb98f3cea44873cf56db7
eftanzer <ezra.tanzer@snyk.io>
2025-02-06 10:36:29 -0800

    comment added

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const createToken = (un) => { return 'abc123' }
 
 app.use(csurf())
 app.use(session({
+  //Comment again
   secret: 'keyboard cat',
   name: 'my-session',
   cookie: { path: '/', secure: true }
