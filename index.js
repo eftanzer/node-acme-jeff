cef09d72f6a119b1e831cf3bf493a87966426968
eftanzer <ezra.tanzer@snyk.io>
2025-02-06 10:38:44 -0800

    don't remember if I added or removed the comment

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const createToken = (un) => { return 'abc123' }
 
 app.use(csurf())
 app.use(session({
-  //Comment again
   secret: 'keyboard cat',
   name: 'my-session',
   cookie: { path: '/', secure: true }
